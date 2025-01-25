a0ac25ec7e7418eb82ac7e5d4b789fbe76b69790
Sameer D'Costa <sameerdcosta@gmail.com>
2025-01-25 11:21:40 -0600

    Option to run inside container

--- hpc/data_pipeline.sh
+++ hpc/data_pipeline.sh
@@ -2,29 +2,49 @@
 
 #set -x
 readonly CWD=$PWD
+readonly STAGING_DIR=/staging/groups/glbrc_alphafold/af3
 
-readonly SINGIMG=alphafold3.sif
+DB_DIR_STUB=db
+SINGIMG=""
 
-PRINT_INFO=$1
+PRINT_INFO=0
 function printinfo {
   if [[ "$PRINT_INFO" -ne 0 ]] ; then
     echo $1
   fi
 }
-printinfo "Checking if printinfo is on"
 
-readonly STAGING_DIR=/staging/groups/glbrc_alphafold/af3
-if [[ "$2" -eq 0 ]];
-then
-  readonly DB_DIR_STUB=db
-else
-  readonly DB_DIR_STUB=db_small
-fi
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -p|--printinfo)
+      PRINT_INFO=1
+      printinfo "Setting printinfo on"
+      shift # past argument
+      ;;
+    -r|--run_in_container)
+      SINGIMG="$2"
+      printinfo "Will copy container $SINGIMG and run inside it"
+      shift # past argument
+      shift # past value
+      ;;
+    --smalldb)
+      DB_DIR_STUB=db_small
+      shift # past argument
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+  esac
+done
+
+
 printinfo "Setting up database : $DB_DIR_STUB"
 
 readonly STAGING_DB_DIR="${STAGING_DIR}/${DB_DIR_STUB}"
 
-mkdir work
+mkdir -p work
 pushd work
 
 mkdir -p af_input af_output models public_databases
@@ -32,16 +52,22 @@ mkdir -p af_input af_output models public_databases
 # prepare input directory
 mv ../*.json af_input/
 
-## copy the container
-cp "${STAGING_DIR}"/${SINGIMG} .
+## copy the container if we are going to run commands inside it
+IMGEXEC="" # default is to not pipe commands through container
+if [[ -n "$SINGIMG" ]] ; then
+  printinfo "Copying container : $SINGIMG"
+  cp "${STAGING_DIR}"/${SINGIMG} .
+  IMGEXEC="apptainer exec ${SINGIMG}"
+else
+  printinfo "Running inside the container : not copying container"
+fi
 
 ## prepare public_databases directory
+## Will do 8 file copy/uncompress in parallel so we request 8 cpus
 printinfo "Decompressing pdb_2022_09_28_mmcif_files"
 cat "${STAGING_DB_DIR}"/pdb_2022_09_28_mmcif_files.tar.zst | \
-        apptainer exec ${SINGIMG} \
-        tar --no-same-owner --no-same-permissions \
+        ${IMGEXEC} tar --no-same-owner --no-same-permissions \
         --use-compress-program=zstd -xf - --directory=public_databases/ &
-
 
 for NAME in mgy_clusters_2022_05.fa \
             bfd-first_non_consensus_sequences.fasta \
@@ -52,30 +78,41 @@ for NAME in mgy_clusters_2022_05.fa \
             rfam_14_9_clust_seq_id_90_cov_80_rep_seq.fasta ; do
   printinfo "Start decompressing: '${NAME}'"
   cat "${STAGING_DB_DIR}/${NAME}.zst" | \
-      apptainer exec ${SINGIMG}  \
-        zstd --decompress > "public_databases/${NAME}" &
+      ${IMGEXEC} zstd --decompress > "public_databases/${NAME}" &
 done
 
 wait
 printinfo "Completed database installation"
 
-apptainer exec \
-   --bind af_input:/root/af_input \
-   --bind af_output:/root/af_output \
-   --bind models:/root/models \
-   --bind public_databases:/root/public_databases \
-   --cwd /app/alphafold \
-   alphafold3.sif \
-   python run_alphafold.py \
-   --db_dir=/root/public_databases \
-   --model_dir=/root/models \
-   --run_data_pipeline=true \
-   --run_inference=false \
-   --input_dir=/root/af_input \
-   --model_dir=/root/models \
-   --output_dir=/root/af_output 
+if [[ -n "$SINGIMG" ]] ; then
+  apptainer exec \
+    --bind af_input:/root/af_input \
+    --bind af_output:/root/af_output \
+    --bind models:/root/models \
+    --bind public_databases:/root/public_databases \
+    --cwd /app/alphafold \
+    ${SINGIMG} \
+    python run_alphafold.py \
+    --db_dir=/root/public_databases \
+    --run_data_pipeline=true \
+    --run_inference=false \
+    --input_dir=/root/af_input \
+    --model_dir=/root/models \
+    --output_dir=/root/af_output
+else
+  workdir=`realpath .`
+  pushd /app/alphafold
+  python run_alphafold.py \
+       --db_dir=${workdir}/public_databases \
+       --model_dir=${workdir}/models \
+       --run_data_pipeline=true \
+       --run_inference=false \
+       --input_dir=${workdir}/af_input \
+       --output_dir=${workdir}/af_output
+  popd # back to workdir
+fi
 
-popd
+popd # back to home dir
 
 # tar up the output directory - one tar for each job. These get returned
 shopt -s nullglob # we do not want an empty match below
@@ -88,5 +125,5 @@ done
 
 # clean up
 rm -rf work
-rm -rf .bash_history .bashrc .lesshst
+rm -rf .bash_history .bashrc .lesshst .viminfo
 
--- hpc/inference_pipeline.sh
+++ hpc/inference_pipeline.sh
@@ -2,26 +2,51 @@
 
 #set -x
 readonly CWD=$PWD
+readonly STAGING_DIR=/staging/groups/glbrc_alphafold/af3
 
-readonly SINGIMG=alphafold3.sif
+SINGIMG=""
 
-PRINT_INFO=$1
+# this file should be protected by your staging dir unix permissions
+MODEL_PARAM_FILE=/staging/dcosta2/af3/weights/af3.bin
+
+PRINT_INFO=0
 function printinfo {
   if [[ "$PRINT_INFO" -ne 0 ]] ; then
     echo $1
   fi
 }
-printinfo "Checking if printinfo is on"
 
-readonly STAGING_DIR=/staging/groups/glbrc_alphafold/af3
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -p|--printinfo)
+      PRINT_INFO=1
+      printinfo "Setting printinfo on"
+      shift # past argument
+      ;;
+    -r|--run_in_container)
+      SINGIMG="$2"
+      printinfo "Will copy container $SINGIMG and run inside it"
+      shift # past argument
+      shift # past value
+      ;;
+    -m|--model_param_file)
+      MODEL_PARAM_FILE="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+  esac
+done
 
-# this file should be protected by your file permissions
-readonly MODEL_PARAM_FILE=/staging/dcosta2/af3/weights/af3.bin
 
-mkdir work
+
+mkdir -p work
 pushd work
 
-mkdir -p af_input af_output models public_databases 
+mkdir -p af_input af_output models public_databases
 
 # prepare input directory
 for filename in ../*.data_pipeline.tar.gz ;
@@ -31,37 +56,56 @@ do
  rm "${filename}"
 done
 
-## copy the container
-cp "${STAGING_DIR}"/${SINGIMG} .
+## copy the container if we are going to run commands inside it
+IMGEXEC="" # default is to not pipe commands through container
+if [[ -n "$SINGIMG" ]] ; then
+  printinfo "Copying container : $SINGIMG"
+  cp "${STAGING_DIR}"/${SINGIMG} .
+  IMGEXEC="apptainer exec ${SINGIMG}"
+else
+  printinfo "Running inside the container : not copying container"
+fi
 
 if [[ ${MODEL_PARAM_FILE} == *.zst ]]; then
   cat "${MODEL_PARAM_FILE}"  | \
-        apptainer exec ${SINGIMG} \
-        zstd  --decompress > models/af3.bin
-  printinfo "Decompressing model weights"
+        ${IMGEXEC} zstd  --decompress > models/af3.bin
+  printinfo "Decompressing model weights : ${MODEL_PARAM_FILE}"
 else
   cp "${MODEL_PARAM_FILE}" models/af3.bin
-  printinfo "Copying model weights"
+  printinfo "Copying model weights : ${MODEL_PARAM_FILE}"
 fi
 
-apptainer exec \
-   --bind af_input:/root/af_input \
-   --bind af_output:/root/af_output \
-   --bind models:/root/models \
-   --bind public_databases:/root/public_databases \
-   --cwd /app/alphafold \
-   --nv \
-   alphafold3.sif \
-   python run_alphafold.py \
-   --db_dir=/root/public_databases \
-   --model_dir=/root/models \
-   --run_data_pipeline=false \
-   --run_inference=true \
-   --input_dir=/root/af_input \
-   --model_dir=/root/models \
-   --output_dir=/root/af_output 
+if [[ -n "$SINGIMG" ]] ; then
+  apptainer exec \
+     --bind af_input:/root/af_input \
+     --bind af_output:/root/af_output \
+     --bind models:/root/models \
+     --bind public_databases:/root/public_databases \
+     --cwd /app/alphafold \
+     --nv \
+     ${SINGIMG} \
+     python run_alphafold.py \
+     --db_dir=/root/public_databases \
+     --model_dir=/root/models \
+     --run_data_pipeline=false \
+     --run_inference=true \
+     --input_dir=/root/af_input \
+     --output_dir=/root/af_output
+else
+  workdir=`realpath .`
+  pushd /app/alphafold
+  python run_alphafold.py \
+       --db_dir=${workdir}/public_databases \
+       --model_dir=${workdir}/models \
+       --run_data_pipeline=false \
+       --run_inference=true \
+       --input_dir=${workdir}/af_input \
+       --output_dir=${workdir}/af_output
+  popd # back to workdir
+fi
 
-popd
+popd # back to home dir
+
 
 # tar up the output directory - one tar for each job. These get returned
 
@@ -75,5 +119,5 @@ done
 
 # clean up
 rm -rf work
-rm -rf .bash_history .bashrc .lesshst
+rm -rf .bash_history .bashrc .lesshst .viminfo
 
